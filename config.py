bb3e938e741e9683a6683a794df871f943855457
bandaleomug <166202716+dhwlgjs091221@users.noreply.github.com>
2025-06-03 09:14:07 +0900

    Update and rename config.py to test.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Configuration example
-SUPABASE_URL = "https://dldyccaammfychjyttlw.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsZHljY2FhbW1meWNoanl0dGx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NTg1MzYsImV4cCI6MjA2NDQzNDUzNn0.7CtgpVEF7t-aNKjoys9aSG5dXxhzQcaQm1ngcHlHbgs"
-STORAGE_BUCKET = "character-images"
-WEBSOCKET_SERVER_URL = "wss://trpgapp.up.railway.app"
