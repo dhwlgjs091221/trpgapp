b5bd4945c21a7f93b39acca8320f02a44b3a1c4c
bandaleomug <166202716+dhwlgjs091221@users.noreply.github.com>
2025-06-03 10:32:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from character_creator import character_creator_page
 from websocket_client import setup_websocket
-from config import SUPABASE_URL, SUPABASE_KEY, SUPABASE_BUCKET, WEBSOCKET_SERVER_URL
+from con import SUPABASE_URL, SUPABASE_KEY, SUPABASE_BUCKET, WEBSOCKET_SERVER_URL
 
 st.set_page_config(layout="wide")
 
